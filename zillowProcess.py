b5cf748e5876818071e690919b5c36e8c209f31d
Arunav Gupta <centaurus.ab@gmail.com>
2019-11-01 13:05:45 -0700

    repurposed to clean data

--- zillowProcess.py
+++ zillowProcess.py
@@ -4,27 +4,29 @@ from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 
-city = pd.read_csv("City_time_series.csv")
+city = pd.read_csv("./data/City_time_series.csv")
 prefilter_shape = city.shape[0]
+print(city.columns)
 
 # Begin filtering
 # create subtable with just 3bedroom prices, cityID, and Date
 # add new column for true if a 3bedroom price exists for that row (use isnull)
 # group each cityId by number of true values (see last dsc10 lecture for how to do this)
-cols = ["Date", "RegionName", "MedianListingPricePerSqft_3Bedroom"]
+cols = ["Date", "RegionName", "MedianListingPricePerSqft_1Bedroom", "MedianListingPricePerSqft_2Bedroom", "MedianListingPricePerSqft_3Bedroom", "MedianListingPricePerSqft_4Bedroom", "MedianListingPricePerSqft_5BedroomOrMore"]
 city_filter1 = city.filter(cols)
-city_filter1 = city_filter1[city_filter1["MedianListingPricePerSqft_3Bedroom"].isnull() == False]
+for i in tqdm(range(1, 5)):
+    city_filter1 = city_filter1[city_filter1["MedianListingPricePerSqft_" + str(i) + "Bedroom"].isnull() == False]
+city_filter1 = city_filter1[city_filter1["MedianListingPricePerSqft_5BedroomOrMore"].isnull() == False]
 cityList = np.unique(city_filter1['RegionName'].values)
 postfilter_shape = city_filter1.shape[0]
 
-# Get cities with most data on 3bedroom prices
-city_filter1 = city_filter1.groupby("RegionName").count()
-city_filter1['Region'] = cityList
-city_filter1 = city_filter1.sort_values("MedianListingPricePerSqft_3Bedroom", ascending=False)
-
 print(city_filter1.head())
 print(city_filter1.shape)
+print(city_filter1.sort_values('Date', ascending=True).head())
 print("Filtered", (prefilter_shape - postfilter_shape), "values")
 
-leading_cities = city_filter1[city_filter1['MedianListingPricePerSqft_3Bedroom'] == 96]
-print(leading_cities.shape)
+# city_filter2 = city_filter1[city_filter1['RegionName'] == 'miami_beachmiami_dadefl']
+# print(city_filter2.sort_values('Date', ascending=False).head())
+#
+# plt.plot(city_filter2['Date'], city_filter2['MedianListingPricePerSqft_5BedroomOrMore'])
+# plt.show()
